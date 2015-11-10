35c7174cfa106f49047dff55f37b5bba98404bfe
august-zou <zjzouxu@gmail.com>
2015-11-10 17:01:47 +0800

    add all

--- voting-client/test/components/Results_spec.jsx
+++ voting-client/test/components/Results_spec.jsx
@@ -29,11 +29,25 @@ describe('Results', () => {
 
     const pair=List.of("Trainspotting", "28 Days Later");
     const component = renderIntoDocument(
-      <Results pair={pair} tally={tally} next={next}/>
+      <Results pair={pair} tally={Map()} next={next}/>
     );
 
     Simulate.click(React.findDOMNode(component.refs.next));
 
     expect(nextInvoked).to.equal(true);
   });
+  
+  it('renders the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Results winner="Trainspotting"
+               pair={["Trainspotting", "28 Days Later"]}
+               tally={Map()} /> 
+    );
+
+    const winner = React.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain("Trainspotting");
+  });
+
+
 });
